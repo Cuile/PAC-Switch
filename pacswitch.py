72634ff9165077245566b7d5e01415ab094668f1
Cuile <i@cuile.com>
2025-01-08 17:46:27 +0800

    优化代码

--- pacswitch.py
+++ pacswitch.py
@@ -13,7 +13,7 @@ def open_pac_proxy() -> None:
     with winreg.OpenKey(reg_key, reg_sub_key, 0, winreg.KEY_SET_VALUE) as key:
         winreg.SetValueEx(key, reg_value_name, 0, winreg.REG_SZ, pac_script)
         winreg.FlushKey(key)
-        print("PAC打开成功")
+        print("PAC打开成功，按任何键退出 & 关闭PAC。")
 
 
 def close_pac_proxy() -> None:
@@ -21,7 +21,7 @@ def close_pac_proxy() -> None:
         try:
             winreg.DeleteValue(key, reg_value_name)
             winreg.FlushKey(key)
-            print("PAC关闭成功")
+            print("PAC关闭成功。")
         except FileNotFoundError:
             # 如果注册表值已经被删除，则什么也不做
             pass
@@ -33,10 +33,11 @@ def signal_handler(signum, frame):
 
 if __name__ == "__main__":
     try:
-        open_pac_proxy()
         signal.signal(signal.SIGTERM, signal_handler)
-        print("按任何键，退出 & 关闭PAC")
+        open_pac_proxy()
         os.system("pause")
-        close_pac_proxy()
     except SystemExit:
+        # close_pac_proxy()
+        pass
+    finally:
         close_pac_proxy()
